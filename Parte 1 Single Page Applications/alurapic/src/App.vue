2bfd3ef505d7c2ed6c8ebf3e9c5e8c93fcc00bc8
Dudu Tavares <skynetdudu@icloud.com>
2021-10-17 14:57:34 -0300

    fim da aula 1

---
+++ Parte 1 Single Page Applications/alurapic/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ titulol }}</h1>
+    <img
+      src="https://www.infomoney.com.br/wp-content/uploads/2019/06/homer-simpson.jpg?fit=900%2C734&quality=50&strip=all"
+      alt="homer-simpson"
+    />
+  </div>
+</template>
+
+
+
+
+
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      titulol: "alurapic",
+    };
+  },
+};
+</script>
+
+
+
+
+
+<style>
+</style>
